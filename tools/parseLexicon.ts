376bc3b20d8ac13831c78fbf2f461599c30f1228
bedorlan <bedorlan@gmail.com>
2021-04-12 19:28:44 -0500

    added lexicon.json

---
+++ tools/parseLexicon.ts
@@ -0,0 +1,15 @@
+import fs = require('fs')
+
+const lexicon = fs.readFileSync(0, 'utf-8')
+const [head, patterns, end] = lexicon.split('-'.repeat(71))
+const extractPattern = /:(.*?):(.*?(?=(?:\n:|\t|\s*$)))((\t[^\s]*\n)*)/gs
+const result = []
+for (const pattern of patterns.matchAll(extractPattern)) {
+  const [, name, desc, grid] = pattern
+  const parsedGrid = grid
+    .split('\n')
+    .map(g => g.trim().replace(/[^.]/g, '1').replace(/[.]/g, '0'))
+    .filter(Boolean)
+  result.push({ name, desc, grid: parsedGrid })
+}
+console.log(JSON.stringify(result, null, 2))
